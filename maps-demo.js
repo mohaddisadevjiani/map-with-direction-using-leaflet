b55284920c4f6901471ad7ae041ae4e5d9255c77
mohaddisadevjiani <60886782+mohaddisadevjiani@users.noreply.github.com>
2023-01-16 10:26:09 +0000

    Add files via upload

---
+++ maps-demo.js
@@ -0,0 +1,17 @@
+var map = L.map( 'map', {
+    center: [20.0, 5.0],
+    minZoom: 2,
+    zoom: 2
+});
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo( map );
+
+for ( var i=0; i < markers.length; ++i ) 
+{
+   L.marker( [markers[i].lat, markers[i].lng] )
+      .bindPopup( '<a href="' + markers[i].url + '" target="_blank" rel="noopener">' + markers[i].name + '</a>' )
+      .addTo( map );
+}
